21e58ec78b5e66604c6e8a48aaa4aabf679a81e2
jbedia <joaquin.bedia@gmail.com>
2014-06-18 14:20:47 +0200

    startup messages modified

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,9 +1,5 @@
 .onAttach <- function(...) {
     mess1 <- "NetCDF Java Library loaded and ready"
-    mess2 <- "INFO: http://www.unidata.ucar.edu/software/thredds/current/netcdf-java/documentation.htm"
-    mess3 <- "LICENSE: http://www.unidata.ucar.edu/software/netcdf/copyright.html"
     packageStartupMessage(mess1)
-    packageStartupMessage(mess2)
-    packageStartupMessage(mess3)
 } 
 
