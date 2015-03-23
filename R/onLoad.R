040c4b231f6a8f3f20fc0edba0f595931a1ad3a7
jbedia <joaquin.bedia@gmail.com>
2015-03-23 18:28:24 +0100

    Testing netCDF Java v4.6.0-SNAPSHOT

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,5 +1,4 @@
-.onLoad <- function(libname, pkgname = "netcdfAll-4.3.jar") {
+.onLoad <- function(libname, pkgname = "netcdfAll-4.6.0-SNAPSHOT.jar") {
       .jpackage(pkgname, lib.loc = libname)
       options(java.parameters = "-Xmx2g")
 }
-
