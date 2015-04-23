618013ff6536aec4fc92f45d51bfb52223e2e333
jbedia <joaquin.bedia@gmail.com>
2015-04-23 18:15:31 +0200

    Update onAttach

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,5 +1,5 @@
 .onAttach <- function(...) {
-    mess1 <- "NetCDF Java Library v4.6.0-SNAPSHOT (23 Mar 2015) loaded and ready"
+    mess1 <- "NetCDF Java Library v4.6.0-SNAPSHOT (23 Apr 2015) loaded and ready"
     packageStartupMessage(mess1)
     J("java.util.logging.Logger")$getLogger("")$setLevel(J("java.util.logging.Level")$SEVERE)
 } 
